07e3faeecd14dfa9fcd96e6002b0f071e4757b79
Luan-Felipe-R-Sanches <luenfeliperodrigues25@gmail.com>
2023-12-08 16:59:17 -0400

    Orientação a Objetos

---
+++ 13-orientacao_objetos/classe_celular.py
@@ -0,0 +1,20 @@
+# Classe Celualar:
+#  Atributos:
+#   Fabricante: String
+#   Modelo: String
+#   Tela: Decimal
+#   Armazenamento: Inteiro
+#   Memória: Inteiro
+#   Câmera: Inteiro
+#   Bateria: Inteiro
+#   Tela ligada: Booleano
+# Métodos:
+#   ligar_tela()
+#   salvar_em_disco()
+#   carregar_aplicativo()
+#   tirar_foto()
+#   verificar_carga_bateria()
+
+class Celular:
+    def ligar_tela(self):
+
