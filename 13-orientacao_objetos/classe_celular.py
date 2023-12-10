60d0bb61360a0acce3b2d3c3c64cf62c4c5d98eb
luanf-asus-big-linux <luanfeliperodrigues25@gmail.com>
2023-12-10 11:58:24 -0400

    Classe Celular

--- 13-orientacao_objetos/classe_celular.py
+++ 13-orientacao_objetos/classe_celular.py
@@ -14,7 +14,35 @@
 #   carregar_aplicativo()
 #   tirar_foto()
 #   verificar_carga_bateria()
+from random import random
+
 
 class Celular:
-    def ligar_tela(self):
+    def __init__(self, fabricante, modelo, tela, armazenamento, memoria, camera, bateria, tela_ligada):
+        self.fabricante = fabricante
+        self.modelo = modelo
+        self.tela = tela
+        self.armazenamento = armazenamento
+        self.memoria = memoria
+        self.camera = camera
+        self.bateria = bateria
+        self.tela_ligada = tela_ligada
 
+    def verificar_carga_bateria(self):
+        # random() retorna um float de [0, 1)
+
+     porcentagem_bateria = random()
+     carga_atual = self.bateria * porcentagem_bateria
+     bateria_restante = self.bateria - carga_atual
+     print(f'O celular está com {porcentagem_bateria:.0f}% de bateria e {bateria_restante:.0f}mA restantes')
+
+    def ligar_tela(self):
+        self.tela_ligada = True
+
+celular_android = Celular("Samsung", "S10", 6.25, 128, 4, 21,
+                          3400, False)
+celular_iphone = Celular(fabricante="Apple", modelo="Iphone 13 PRO", tela=5.75, armazenamento=128, memoria=3, camera=16,
+                         bateria=2650, tela_ligada=False)
+
+celular_iphone.ligar_tela()
+print(celular_iphone.tela_ligada)
